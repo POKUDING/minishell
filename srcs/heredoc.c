28add55d43a6124e70152ba19aa65c083b2a757e
Junhyung Park <junhyupa@c1r4s1.42seoul.kr>
2023-03-07 16:49:09 +0900

    workin heredoc.c

--- srcs/heredoc.c
+++ srcs/heredoc.c
@@ -6,16 +6,20 @@
 /*   By: junhyupa <junhyupa@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/05 19:13:02 by junhyupa          #+#    #+#             */
-/*   Updated: 2023/03/05 21:12:40 by junhyupa         ###   ########.fr       */
+/*   Updated: 2023/03/07 16:48:54 by junhyupa         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../minishell.h"
 
-void	here_doc(t_token *token)
+void	here_doc(t_token *token, char *eof)
 {
 	char	*s;
+	int		fd[2];
 
 	s = readline("<");
-	while()
+	pipe(&fd);
+	dup2(fd[0], 0);
+	while(ft_strncmp(s, eof, sizeof(eof)))
+		write(fd[1], s, ft_strlen(s));
 }
